cf9cb3b68f9d44524aee6470b226578dcc8a9501
andrmantz <75033264+andrmantz@users.noreply.github.com>
2025-02-02 15:46:27 +0200

    Add b_rate mock pool and improve default fee_vault constructor values

--- src/testutils.rs
+++ src/testutils.rs
@@ -18,7 +18,11 @@ pub(crate) fn register_fee_vault(
 ) -> Address {
     e.register(
         FeeVault {},
-        constructor_args.unwrap_or((Address::generate(e), Address::generate(e), 100_0000)),
+        constructor_args.unwrap_or((
+            Address::generate(e),
+            mockpool::register_mock_pool_with_b_rate(e, 1_100_000_000).address,
+            0_1000000,
+        )),
     )
 }
 
@@ -240,4 +244,68 @@ pub fn create_mock_oracle<'a>(e: &Env) -> (Address, MockPriceOracleClient<'a>) {
         contract_id.clone(),
         MockPriceOracleClient::new(e, &contract_id),
     )
+}
+
+/// Mock pool to test b_rate updates
+pub mod mockpool {
+
+    use super::*;
+    use soroban_sdk::{contract, contractimpl, contracttype, symbol_short, Symbol};
+
+    const BRATE: Symbol = symbol_short!("b_rate");
+    #[derive(Clone)]
+    #[contracttype]
+    pub struct Reserve {
+        pub asset: Address,        // the underlying asset address
+        pub index: u32,            // the reserve index in the pool
+        pub l_factor: u32,         // the liability factor for the reserve
+        pub c_factor: u32,         // the collateral factor for the reserve
+        pub max_util: u32,         // the maximum utilization rate for the reserve
+        pub last_time: u64,        // the last block the data was updated
+        pub scalar: i128,          // scalar used for positions, b/d token supply, and credit
+        pub d_rate: i128,          // the conversion rate from dToken to underlying (9 decimals)
+        pub b_rate: i128,          // the conversion rate from bToken to underlying (9 decimals)
+        pub ir_mod: i128,          // the interest rate curve modifier (9 decimals)
+        pub b_supply: i128,        // the total supply of b tokens
+        pub d_supply: i128,        // the total supply of d tokens
+        pub backstop_credit: i128, // the total amount of underlying tokens owed to the backstop
+        pub collateral_cap: i128, // the total amount of underlying tokens that can be used as collateral
+        pub enabled: bool,        // is the reserve enabled
+    }
+    #[contract]
+    pub struct MockPool;
+
+    #[contractimpl]
+    impl MockPool {
+        pub fn set_b_rate(e: Env, b_rate: i128) {
+            e.storage().instance().set(&BRATE, &b_rate);
+        }
+
+        pub fn get_reserve(e: Env, _reserve: Address) -> Reserve {
+            Reserve {
+                asset: e.current_contract_address(),
+                index: 0,
+                l_factor: 0,
+                c_factor: 0,
+                max_util: 0,
+                last_time: 0,
+                scalar: 0,
+                d_rate: 0,
+                b_rate: e.storage().instance().get(&BRATE).unwrap_or(1_000_000_000),
+                ir_mod: 0,
+                b_supply: 0,
+                d_supply: 0,
+                backstop_credit: 0,
+                collateral_cap: 0,
+                enabled: true,
+            }
+        }
+    }
+
+    pub fn register_mock_pool_with_b_rate(e: &Env, b_rate: i128) -> mockpool::MockPoolClient {
+        let pool_address = e.register(mockpool::MockPool {}, ());
+        let client = mockpool::MockPoolClient::new(e, &pool_address);
+        client.set_b_rate(&b_rate);
+        client
+    }
 }
